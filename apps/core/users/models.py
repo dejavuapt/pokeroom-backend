6d20325b2cdd094b25e1e707fcc309785fbf0874
dejavuapt <thepoli.b@gmail.com>
2025-05-10 22:15:58 +0500

    feat(users): Created model PokeroomUser with fields

--- apps/core/users/models.py
+++ apps/core/users/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext as _
+from django.utils import timezone
 
 class PokeroomUser(AbstractUser):
     
@@ -12,7 +13,35 @@ class PokeroomUser(AbstractUser):
             "unique": _("A user with that username already exists."),
         },
     )
-    
-    first_name = models.CharField(_("first name"), max_length=150, blank=True)
+    first_name = models.CharField(_("first name"), max_length=150)
     last_name = models.CharField(_("last name"), max_length=150, blank=True)
-    email = models.EmailField(_("email address"), blank=True)
+    email = models.EmailField(
+        _("email address"),
+        unique=True,
+        error_messages={
+            "unique": _("A user with that email already exists."),
+        },
+    )
+    image_url = models.ImageField(_("image url"),null=True,default=None,blank=True)
+    created_at = models.DateTimeField(_("created at"),default=timezone.now)
+    telegram_id = models.CharField("telegram id",max_length=20,null=True,default=None,blank=True)
+    
+    class Meta:
+        verbose_name = "puser"
+        verbose_name_plural = "pusers"
+        
+    def __str__(self):
+        return "%s (%s)" % (self.get_full_name(), self.get_username())
+    
+    # GETTERS
+    
+    def get_image_url(self):
+        return getattr(self, "image_url")
+    
+    def get_when_created(self):
+        return getattr(self, "created_at")
+    
+    def get_telegram_id(self):
+        return getattr(self, "telegram_id")
+    
+    
