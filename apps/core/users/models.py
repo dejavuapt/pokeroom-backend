4abb59a5351cd2bf4eac52226da9b1f770e4021f
dejavuapt <thepoli.b@gmail.com>
2025-05-10 21:54:08 +0500

    feat(users): Переопределена абстрактная модель для юзера

--- apps/core/users/models.py
+++ apps/core/users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext as _
 
-# Create your models here.
+class PokeroomUser(AbstractUser):
+    
+    username = models.CharField(
+        verbose_name = _("username"),
+        max_length=50,
+        unique=True,
+        error_messages={
+            "unique": _("A user with that username already exists."),
+        },
+    )
+    
+    first_name = models.CharField(_("first name"), max_length=150, blank=True)
+    last_name = models.CharField(_("last name"), max_length=150, blank=True)
+    email = models.EmailField(_("email address"), blank=True)
